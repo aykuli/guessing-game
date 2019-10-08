ed2c0eb576acf2f61611b948d25c13b81c68aff3
Aykuli <aykuli@ya.ru>
2019-10-08 13:38:15 +0700

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max - this.min) / 2 + this.min);
     }
 
     lower() {
-
+        this.max = this.guess();
+        return Math.round((this.max - this.min) / 2 + this.min);
     }
 
     greater() {
-
+        this.min = this.guess();
+        return Math.round((this.max - this.min) / 2 + this.min);
     }
 }
 
